79967811ffdd739bd7a653f4644eec5c4b014625
Kevin Conway <kevinjacobconway@gmail.com>
2016-03-27 08:44:44 -0500

    Fix packaging to resolve the PEP420 namespace
    Setuptools is still lacking support for PEP480 namespace packages
    when using the find_packages function. Until it does all packages,
    including the namespace, must be registered in the packages list.

--- setup.py
+++ setup.py
@@ -10,14 +10,18 @@ with open('README.rst', 'r') as readmefile:
 
 setup(
     name='asyncdef.interfaces',
-    version='0.1.0',
+    version='0.1.3',
     url='https://github.com/asyncdef/interfaces',
     description='Public APIs for the core asyncdef components.',
     author="Kevin Conway",
     author_email="kevinjacobconway@gmail.com",
     long_description=README,
     license='Apache 2.0',
-    packages=find_packages(exclude=['build', 'dist', 'docs']),
+    packages=[
+        'asyncdef',
+        'asyncdef.interfaces',
+        'asyncdef.interfaces.engine',
+    ],
     install_requires=[
         'iface',
     ],
